d780afdf144d15ec54dd152224f06b656f440fdc
hemypo-star <hemypo@gmail.com>
2026-09-19 23:43:34 +0700

    fix: match prototype admin KPI responsive grid

--- apps/cms/src/components/admin/AdminKpiCards.tsx
+++ apps/cms/src/components/admin/AdminKpiCards.tsx
@@ -20,7 +20,7 @@ export default function AdminKpiCards({ kpi }: { kpi: AdminKpi }) {
   ]
 
   return (
-    <div className="grid grid-cols-1 gap-3.5 sm:grid-cols-2 lg:grid-cols-4">
+    <div className="pb-admin-kpis">
       {cards.map((c, i) => (
         <div
           key={c.label}
