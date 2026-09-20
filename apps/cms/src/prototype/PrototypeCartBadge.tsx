d73a05b7de58de9e48626eb662bbcc61322f411e
hemypo-star <hemypo@gmail.com>
2026-09-20 14:06:23 +0700

    feat: replay prototype cart badge pop

--- apps/cms/src/prototype/PrototypeCartBadge.tsx
+++ apps/cms/src/prototype/PrototypeCartBadge.tsx
@@ -5,4 +5,5 @@ import { $cartCount } from '../stores/cart'
 export default function PrototypeCartBadge(){const count=useStore($cartCount);const[hydrated,setHydrated]=useState(false);
  // eslint-disable-next-line react-hooks/set-state-in-effect
  useEffect(()=>setHydrated(true),[]);
- return <span className="pb-cart-count">{hydrated?count:0}</span>}
+ const shown=hydrated?count:0
+ return <span key={shown} className="pb-cart-count">{shown}</span>}
